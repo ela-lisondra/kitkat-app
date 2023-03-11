f95b5d108b2a714824e0d9148872314686308873
ela_lisondra <emrlisondra@gmail.com>
2023-03-11 16:46:03 +0000

    create note feature, compose, home

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { title } = require("process");
 
-const homeStartingContent = "In here life is beautiful but we will give you notes, constructive notes.";
-const rehearsalsContent = "Never ending rehearsals.";
-const notesContent = "Never ending notes.";
+const homeStartingContent = "This is where the show notes will appear.";
+const rehearsalsContent = "In here, we're always rehearsing.";
+const notesContent = "Cute notes.";
 
 const app = express();
+const posts = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -16,17 +19,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent})
-
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  })
 });
 
 app.get("/rehearsals", function(req, res){
   res.render("rehearsals", {startingContent: rehearsalsContent});
-})
+});
 
 app.get("/notes", function(req, res){
-  res.render("notes", {startingContent: notesContent});
-})
+  res.render("notes", {
+    startingContent: notesContent,
+    listTitle: "Notes",
+    newListItems: workItems
+  });
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+    workItems.push(item);
+    res.redirect("/notes");
+ 
+});
+
+app.get("posts/:post")
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  }
+  posts.push(post);
+  res.redirect("/")
+});
+
 
 
 
